3b1935e1c9e3f036e83a74ca258a5c7adcee4565
Jann Dave Apohin <jdapohin@gmail.com>
2023-02-03 13:52:28 +0800

    last na

---
+++ php/connect.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    //DATABASE CONNECTION
+    $conn = new mysqli('localhost','root','','contact_form');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+
+    }else{
+        $stmt = $conn->prepare("insert into registration(name, email, message)values(?, ?, ?)");
+        $stmt->bind_param("sss",$name, $email, $message);
+        $stmt->execute();
+        echo
+        $stmt->close();
+        $conn->close();
+    }
+?>
+
